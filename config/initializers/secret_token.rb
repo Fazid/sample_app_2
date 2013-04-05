ecfc3fa579cd6b5fc765883c9e4ca50fb0099440
Fazid <fazid_3@rambler.ru>
2013-04-05 17:35:47 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GsmTerminal::Application.config.secret_token = 'b041368c2ea87d7ad09acbf44f2461088930da7b6d1d88187cfbbf879c16367ab3d9a3517294b4bd8ae1228f829430c8142be32ee109f17d4884ba3b1001eb60'
